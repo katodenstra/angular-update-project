84fff7b9c6798861bbdf45e93b20142deb87a4a9
Mauricio Villada <mvillada@mac.home>
2025-09-21 23:49:49 +0100

    Initial commit: Angular update project

---
+++ src/app/tasks/tasks.component.ts
@@ -0,0 +1,39 @@
+import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { TaskComponent } from './task/task.component';
+import { NewTaskComponent } from './new-task/new-task.component';
+import { type NewTaskData } from './task/task.model';
+import { TasksService } from './tasks.service';
+
+@Component({
+  selector: 'app-tasks',
+  standalone: true,
+  imports: [TaskComponent, NewTaskComponent],
+  templateUrl: './tasks.component.html',
+  styleUrl: './tasks.component.css',
+})
+export class TasksComponent {
+  @Input({ required: true }) userId!: string;
+  @Input({ required: true }) name?: string;
+
+  @Output() addTask = new EventEmitter<void>();
+
+  isAddingTask = false;
+
+  constructor(private tasksService: TasksService) {}
+
+  get selectedUserTasks() {
+    return this.tasksService.getUserTasks(this.userId);
+  }
+
+  onAddTask(taskData: NewTaskData) {
+    this.isAddingTask = false;
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
+  }
+
+  onCloseAddTask() {
+    this.isAddingTask = false;
+  }
+}
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,37 @@
+import { Component, Input, Output, EventEmitter  } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+import { User } from './user.model';
+import { CardComponent } from "../shared/card/card.component";
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [CardComponent],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+ 
+@Input({required: true}) user!: User;
+@Input({required: true}) selected!: boolean;
+
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+  // id = input.required<string>();
+
+  @Output() select = new EventEmitter<string>();
+
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar;
+  }
+
+  // imagePath = computed(() => {
+  //   return 'assets/users/' + this.avatar();
+  // });
+
+  onSelectUser() {
+    //this.select.emit(this.id);
+    this.select.emit(this.user.id);
+  }
+}
